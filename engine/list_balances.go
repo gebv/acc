f063e564eb9276ea951f4110f4be155f27008a5f
George BV <egor.workroom@gmail.com>
2019-06-23 03:15:40 +0600

    Добавлена вспомогательная функция для listBalances

--- engine/list_balances.go
+++ engine/list_balances.go
@@ -2,6 +2,10 @@ package engine
 
 type listBalances map[int64]int64
 
+func (b listBalances) Get(id int64) int64 {
+	return b[id]
+}
+
 func (b listBalances) Inc(id int64, amount int64) {
 	b[id] += amount
 }
