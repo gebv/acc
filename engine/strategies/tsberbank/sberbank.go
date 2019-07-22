1bec146b2cba7a41234161727d4df62f995ecf88
AndreyDyubin <d.andrey@fader.site>
2019-07-23 02:25:26 +0300

    описание стратегии для провайдера сбербанк

--- engine/strategies/tsberbank/sberbank.go
+++ engine/strategies/tsberbank/sberbank.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"log"
 	"sync"
+	"time"
 
 	"github.com/gebv/acca/engine"
 	"github.com/gebv/acca/engine/strategies"
 	"github.com/gebv/acca/ffsm"
+	"github.com/gebv/acca/provider/sberbank"
 	"github.com/pkg/errors"
 )
 
@@ -24,6 +26,7 @@ func init() {
 type Strategy struct {
 	s        ffsm.Stack
 	syncOnce sync.Once
+	config   sberbank.Config
 }
 
 func (s *Strategy) Name() strategies.TrStrategyName {
@@ -73,7 +76,6 @@ func (s *Strategy) load() {
 			ffsm.State(engine.DRAFT_TX),
 			ffsm.State(engine.AUTH_TX),
 			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
 				trID, ok := payload.(int64)
 				if !ok {
 					log.Println("Transaction bad Payload: ", payload)
@@ -93,28 +95,108 @@ func (s *Strategy) load() {
 				if tr.Status != engine.DRAFT_TX {
 					return ctx, errors.New("Transaction status not draft.")
 				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
+				}
 				// Установить статус куда происходит переход
 				ns := engine.AUTH_TX
 				tr.NextStatus = &ns
+				tr.Status = engine.WAUTH_TX
 				if err := tx.Save(&tr); err != nil {
 					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
 				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
-				tr.Status = engine.AUTH_TX
-				tr.NextStatus = nil
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				err := nc.Publish(sberbank.SUBJECT, &sberbank.MessageToSberbank{
+					Command:       sberbank.AuthTransfer,
+					TransactionID: tr.TransactionID,
+					Strategy:      tr.Strategy,
+					Status:        engine.AUTH_TX,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
 				}
 				return ctx, nil
 			},
 			"draft>auth",
 		)
 		s.s.Add(
+			ffsm.State(engine.WAUTH_TX),
+			ffsm.State(engine.AUTH_TX),
+			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
+				trID, ok := payload.(int64)
+				if !ok {
+					log.Println("Transaction bad Payload: ", payload)
+					return
+				}
+				tx := strategies.GetTXContext(ctx)
+				if tx == nil {
+					return ctx, errors.New("Not reform tx in context.")
+				}
+				tr := engine.Transaction{TransactionID: trID}
+				if err := tx.Reload(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
+				}
+				if tr.Strategy != nameStrategy.String() {
+					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
+				}
+				if tr.Status != engine.WAUTH_TX {
+					return ctx, errors.New("Transaction status not auth_wait.")
+				}
+				inv := engine.Invoice{InvoiceID: tr.InvoiceID}
+				if err := tx.Reload(&inv); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload invoice by ID.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
+				}
+				// Установить статус куда происходит переход
+				tr.Status = engine.WAUTH_TX
+				ns := engine.AUTH_TX
+				tr.NextStatus = &ns
+				if err := tx.Save(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				}
+				err, isHold := engine.ProcessingOperations(tx, &engine.ProcessorCommand{
+					TrID:          tr.TransactionID,
+					CurrentStatus: engine.DRAFT_TX,
+					NextStatus:    engine.AUTH_TX,
+					UpdatedAt:     time.Now(),
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "failed operation process")
+				}
+				// TODO поправить стратегии для обработки как холдированных, так и не холдированных операций
+				//  эту проверку после исправления убрать.
+				if !isHold {
+					return ctx, errors.Wrap(err, "Support only hold operation.")
+				}
+				invStatus := engine.ACCEPTED_I
+				tr.Status = engine.ACCEPTED_TX
+				if isHold {
+					tr.Status = engine.HOLD_TX
+					invStatus = engine.WAIT_I
+				}
+				tr.NextStatus = nil
+				if err := tx.Save(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				}
+				err = nc.Publish(strategies.UPDATE_INVOICE_SUBJECT, &strategies.MessageUpdateInvoice{
+					InvoiceID: inv.InvoiceID,
+					Strategy:  inv.Strategy,
+					Status:    invStatus,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
+				}
+				return ctx, nil
+			},
+			"auth_wait>auth",
+		)
+		s.s.Add(
 			ffsm.State(engine.DRAFT_TX),
 			ffsm.State(engine.REJECTED_TX),
 			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
 				trID, ok := payload.(int64)
 				if !ok {
 					log.Println("Transaction bad Payload: ", payload)
@@ -133,6 +215,110 @@ func (s *Strategy) load() {
 				}
 				if tr.Status != engine.DRAFT_TX {
 					return ctx, errors.New("Transaction status not draft.")
+				}
+				inv := engine.Invoice{InvoiceID: tr.InvoiceID}
+				if err := tx.Reload(&inv); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload invoice by ID.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
+				}
+				// Установить статус куда происходит переход
+				tr.Status = engine.REJECTED_TX
+				tr.NextStatus = nil
+				if err := tx.Save(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				}
+				err := nc.Publish(strategies.UPDATE_INVOICE_SUBJECT, &strategies.MessageUpdateInvoice{
+					InvoiceID: inv.InvoiceID,
+					Strategy:  inv.Strategy,
+					Status:    engine.REJECTED_I,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
+				}
+				return ctx, nil
+			},
+			"draft>rejected",
+		)
+		s.s.Add(
+			ffsm.State(engine.HOLD_TX),
+			ffsm.State(engine.REJECTED_TX),
+			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
+				trID, ok := payload.(int64)
+				if !ok {
+					log.Println("Transaction bad Payload: ", payload)
+					return
+				}
+				tx := strategies.GetTXContext(ctx)
+				if tx == nil {
+					return ctx, errors.New("Not reform tx in context.")
+				}
+				tr := engine.Transaction{TransactionID: trID}
+				if err := tx.Reload(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
+				}
+				if tr.Strategy != nameStrategy.String() {
+					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
+				}
+				if tr.Status != engine.HOLD_TX {
+					return ctx, errors.New("Transaction status not draft.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
+				}
+				// Установить статус куда происходит переход
+				ns := engine.REJECTED_TX
+				tr.NextStatus = &ns
+				tr.Status = engine.WREJECTED_TX
+				if err := tx.Save(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				}
+				err := nc.Publish(sberbank.SUBJECT, &sberbank.MessageToSberbank{
+					Command:       sberbank.ReverseForHold,
+					TransactionID: tr.TransactionID,
+					Strategy:      tr.Strategy,
+					Status:        engine.REJECTED_TX,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
+				}
+				return ctx, nil
+			},
+			"hold>rejected",
+		)
+		s.s.Add(
+			ffsm.State(engine.WREJECTED_TX),
+			ffsm.State(engine.REJECTED_TX),
+			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
+				trID, ok := payload.(int64)
+				if !ok {
+					log.Println("Transaction bad Payload: ", payload)
+					return
+				}
+				tx := strategies.GetTXContext(ctx)
+				if tx == nil {
+					return ctx, errors.New("Not reform tx in context.")
+				}
+				tr := engine.Transaction{TransactionID: trID}
+				if err := tx.Reload(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
+				}
+				if tr.Strategy != nameStrategy.String() {
+					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
+				}
+				if tr.Status != engine.WREJECTED_TX {
+					return ctx, errors.New("Transaction status not rejected_wait.")
+				}
+				inv := engine.Invoice{InvoiceID: tr.InvoiceID}
+				if err := tx.Reload(&inv); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload invoice by ID.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
 				}
 				// Установить статус куда происходит переход
 				ns := engine.REJECTED_TX
@@ -140,22 +326,36 @@ func (s *Strategy) load() {
 				if err := tx.Save(&tr); err != nil {
 					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
 				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
+				err, _ := engine.ProcessingOperations(tx, &engine.ProcessorCommand{
+					TrID:          tr.TransactionID,
+					CurrentStatus: engine.HOLD_TX,
+					NextStatus:    engine.REJECTED_TX,
+					UpdatedAt:     time.Now(),
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "failed operation process")
+				}
 				tr.Status = engine.REJECTED_TX
 				tr.NextStatus = nil
 				if err := tx.Save(&tr); err != nil {
 					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
 				}
+				err = nc.Publish(strategies.UPDATE_INVOICE_SUBJECT, &strategies.MessageUpdateInvoice{
+					InvoiceID: inv.InvoiceID,
+					Strategy:  inv.Strategy,
+					Status:    engine.REJECTED_I,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
+				}
 				return ctx, nil
 			},
-			"draft>rejected",
+			"rejected_wait>rejected",
 		)
 		s.s.Add(
-			ffsm.State(engine.AUTH_TX),
+			ffsm.State(engine.HOLD_TX),
 			ffsm.State(engine.ACCEPTED_TX),
 			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
 				trID, ok := payload.(int64)
 				if !ok {
 					log.Println("Transaction bad Payload: ", payload)
@@ -172,8 +372,63 @@ func (s *Strategy) load() {
 				if tr.Strategy != nameStrategy.String() {
 					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
 				}
-				if tr.Status != engine.AUTH_TX {
-					return ctx, errors.New("Transaction status not auth.")
+				if tr.Status != engine.HOLD_TX {
+					return ctx, errors.New("Transaction status not hold.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
+				}
+				// Установить статус куда происходит переход
+				ns := engine.ACCEPTED_TX
+				tr.NextStatus = &ns
+				tr.Status = engine.WACCEPTED_TX
+				if err := tx.Save(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				}
+				err := nc.Publish(sberbank.SUBJECT, &sberbank.MessageToSberbank{
+					Command:       sberbank.ReverseForHold,
+					TransactionID: tr.TransactionID,
+					Strategy:      tr.Strategy,
+					Status:        engine.REJECTED_TX,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
+				}
+				return ctx, nil
+			},
+			"hold>accepted",
+		)
+		s.s.Add(
+			ffsm.State(engine.WACCEPTED_TX),
+			ffsm.State(engine.ACCEPTED_TX),
+			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
+				trID, ok := payload.(int64)
+				if !ok {
+					log.Println("Transaction bad Payload: ", payload)
+					return
+				}
+				tx := strategies.GetTXContext(ctx)
+				if tx == nil {
+					return ctx, errors.New("Not reform tx in context.")
+				}
+				tr := engine.Transaction{TransactionID: trID}
+				if err := tx.Reload(&tr); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
+				}
+				if tr.Strategy != nameStrategy.String() {
+					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
+				}
+				if tr.Status != engine.WACCEPTED_TX {
+					return ctx, errors.New("Transaction status not rejected_wait.")
+				}
+				inv := engine.Invoice{InvoiceID: tr.InvoiceID}
+				if err := tx.Reload(&inv); err != nil {
+					return ctx, errors.Wrap(err, "Failed reload invoice by ID.")
+				}
+				nc := strategies.GetNatsFromContext(ctx)
+				if nc == nil {
+					return ctx, errors.New("Not nats connection in context.")
 				}
 				// Установить статус куда происходит переход
 				ns := engine.ACCEPTED_TX
@@ -181,139 +436,31 @@ func (s *Strategy) load() {
 				if err := tx.Save(&tr); err != nil {
 					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
 				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
+				err, _ := engine.ProcessingOperations(tx, &engine.ProcessorCommand{
+					TrID:          tr.TransactionID,
+					CurrentStatus: engine.HOLD_TX,
+					NextStatus:    engine.ACCEPTED_TX,
+					UpdatedAt:     time.Now(),
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "failed operation process")
+				}
 				tr.Status = engine.ACCEPTED_TX
 				tr.NextStatus = nil
 				if err := tx.Save(&tr); err != nil {
 					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
 				}
-				return ctx, nil
-			},
-			"auth>accepted",
-		)
-		s.s.Add(
-			ffsm.State(engine.AUTH_TX),
-			ffsm.State(engine.REJECTED_TX),
-			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
-				trID, ok := payload.(int64)
-				if !ok {
-					log.Println("Transaction bad Payload: ", payload)
-					return
-				}
-				tx := strategies.GetTXContext(ctx)
-				if tx == nil {
-					return ctx, errors.New("Not reform tx in context.")
-				}
-				tr := engine.Transaction{TransactionID: trID}
-				if err := tx.Reload(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
-				}
-				if tr.Strategy != nameStrategy.String() {
-					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
-				}
-				if tr.Status != engine.AUTH_TX {
-					return ctx, errors.New("Transaction status not auth.")
-				}
-				// Установить статус куда происходит переход
-				ns := engine.REJECTED_TX
-				tr.NextStatus = &ns
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
-				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
-				tr.Status = engine.REJECTED_TX
-				tr.NextStatus = nil
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
+				err = nc.Publish(strategies.UPDATE_INVOICE_SUBJECT, &strategies.MessageUpdateInvoice{
+					InvoiceID: inv.InvoiceID,
+					Strategy:  inv.Strategy,
+					Status:    engine.ACCEPTED_I,
+				})
+				if err != nil {
+					return ctx, errors.Wrap(err, "Failed publish to nats.")
 				}
 				return ctx, nil
 			},
-			"auth>rejected",
-		)
-		s.s.Add(
-			ffsm.State(engine.DRAFT_TX),
-			ffsm.State(engine.FAILED_TX),
-			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
-				trID, ok := payload.(int64)
-				if !ok {
-					log.Println("Transaction bad Payload: ", payload)
-					return
-				}
-				tx := strategies.GetTXContext(ctx)
-				if tx == nil {
-					return ctx, errors.New("Not reform tx in context.")
-				}
-				tr := engine.Transaction{TransactionID: trID}
-				if err := tx.Reload(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
-				}
-				if tr.Strategy != nameStrategy.String() {
-					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
-				}
-				if tr.Status != engine.DRAFT_TX {
-					return ctx, errors.New("Transaction status not draft.")
-				}
-				// Установить статус куда происходит переход
-				ns := engine.FAILED_TX
-				tr.NextStatus = &ns
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
-				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
-				tr.Status = engine.FAILED_TX
-				tr.NextStatus = nil
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
-				}
-				return ctx, nil
-			},
-			"draft>failed",
-		)
-		s.s.Add(
-			ffsm.State(engine.AUTH_TX),
-			ffsm.State(engine.FAILED_TX),
-			func(ctx context.Context, payload ffsm.Payload) (context context.Context, e error) {
-				// TODO load transaction from BD
-				trID, ok := payload.(int64)
-				if !ok {
-					log.Println("Transaction bad Payload: ", payload)
-					return
-				}
-				tx := strategies.GetTXContext(ctx)
-				if tx == nil {
-					return ctx, errors.New("Not reform tx in context.")
-				}
-				tr := engine.Transaction{TransactionID: trID}
-				if err := tx.Reload(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed reload transaction by ID.")
-				}
-				if tr.Strategy != nameStrategy.String() {
-					return ctx, errors.New("Transaction strategy not TrSberbankStrategy.")
-				}
-				if tr.Status != engine.AUTH_TX {
-					return ctx, errors.New("Transaction status not draft.")
-				}
-				// Установить статус куда происходит переход
-				ns := engine.FAILED_TX
-				tr.NextStatus = &ns
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
-				}
-				// TODO добавить необходимые операции по транзакции.
-				// Установить статус после проделанных операций
-				tr.Status = engine.FAILED_TX
-				tr.NextStatus = nil
-				if err := tx.Save(&tr); err != nil {
-					return ctx, errors.Wrap(err, "Failed save transaction by ID.")
-				}
-				return ctx, nil
-			},
-			"auth>failed",
+			"accepted_wait>accepted",
 		)
 	})
 }
