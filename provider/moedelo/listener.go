eeabccb76654fd4f4d199955b73e00af35123f30
AndreyDyubin <d.andrey@fader.site>
2019-07-26 09:55:48 +0300

    добавляет listener обновления статуса счетов в моем деле

---
+++ provider/moedelo/listener.go
@@ -0,0 +1,167 @@
+package moedelo
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/gebv/acca/engine"
+	"github.com/gebv/acca/engine/strategies"
+	"github.com/gebv/acca/ffsm"
+	"github.com/gebv/acca/provider"
+	"go.uber.org/zap"
+	"gopkg.in/reform.v1"
+)
+
+func (p *Provider) RunCheckStatusListener(ctx context.Context) {
+	_l := p.l.Named("moe_delo_check_status_listener")
+	_l.Info("Started")
+	store := &provider.Store{DB: p.db}
+	tm, err := store.GetLastTimeFromPaymentSystemName(MD)
+	if err != nil {
+		_l.Panic("Failed get last time from noncash", zap.Error(err))
+	}
+	afterDate := *tm
+	var nextDate time.Time
+	var skip bool
+	var push bool
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+	for ctx.Err() == nil {
+		<-ticker.C
+		_l.Debug("Get data in MD")
+		nextDate = time.Now()
+		listBill, err := p.GetListBills(&afterDate, &nextDate)
+		if err != nil {
+			if err == PROVIDER_NOT_SET {
+				_l.Warn("Failed get list bills from moe delo", zap.Error(err))
+				return
+			}
+			_l.Warn("Failed get list bills from moe delo", zap.Error(err))
+			continue
+		}
+		_l.Debug("Get list bills", zap.Int("len", len(listBill.ResourceList)))
+		skip = false
+		for _, bill := range listBill.ResourceList {
+			if skip {
+				break
+			}
+			_l.Debug("Check Bill", zap.Int64("bill_id", bill.ID), zap.Int("bill_status", int(bill.Status)))
+			orderID := newPaymentOrderID(bill.KontragentID, bill.ID)
+			var oldStatus BillStatus
+			billData, err := store.GetByOrderID(orderID)
+			switch err {
+			case reform.ErrNoRows:
+				err = store.NewOrderWithExtUpdate(orderID, MD, strconv.Itoa(int(bill.Status)), nextDate)
+				if err != nil {
+					_l.Warn("Failed set status in bill from moe delo", zap.Error(err))
+					skip = true
+					continue
+				}
+			case nil:
+				if i, err := strconv.Atoi(billData.RawOrderStatus); err == nil {
+					oldStatus = BillStatus(i)
+				} else {
+					_l.Warn("Failed convert raw_order_status from moe delo", zap.Error(err))
+					skip = true
+					continue
+				}
+			default:
+				_l.Warn("Failed get ext order status in bill from moe delo", zap.Error(err))
+				skip = true
+				continue
+			}
+			push = false
+			var status ffsm.State
+			switch bill.Status {
+			case Paid:
+				if oldStatus != Paid {
+					status = ffsm.State(engine.ACCEPTED_TX)
+					push = true
+				}
+			case PartiallyPaid:
+				if oldStatus != PartiallyPaid {
+					status = ffsm.State(engine.HOLD_TX)
+					push = true
+				}
+			}
+
+			if !skip && push {
+				var tr engine.Transaction
+				err := p.db.SelectOneTo(&tr, "WHERE provider = $1 AND provider_oper_id = $2", engine.MOEDELO, strconv.FormatInt(bill.ID, 10))
+				if err != nil {
+					if err == reform.ErrNoRows {
+						_l.Warn("Not found related transactions by bill ID", zap.Int64("bill_id", bill.ID))
+						continue
+					}
+					_l.Warn("Failed find related transactions by bill ID ", zap.Int64("bill_id", bill.ID), zap.Error(err))
+					skip = true
+					continue
+				}
+				statusStr := bill.Status.String()
+				tr.ProviderOperStatus = &statusStr
+				if err := p.db.Save(&tr); err != nil {
+					_l.Warn("Failed save transaction. ", zap.Error(err))
+					skip = true
+					continue
+				}
+				tx, err := p.db.Begin()
+				if err != nil {
+					_l.Error("Failed begin transaction DB.", zap.Error(err))
+					skip = true
+					continue
+				}
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+				ctx = strategies.SetNatsToContext(ctx, p.nc)
+				ctx = strategies.SetTXContext(ctx, tx)
+				if name := strategies.ExistTrName(tr.Strategy); name != strategies.UNDEFINED_TR {
+					if str := strategies.GetTransactionStrategy(name); str != nil {
+						err := str.Dispatch(ctx, status, tr.TransactionID)
+						if err != nil {
+							_l.Error("Failed dispatch transaction strategy. ", zap.Error(err))
+							if err := tx.Rollback(); err != nil {
+								_l.Error("Failed tx rollback. ", zap.Error(err))
+							}
+							skip = true
+							continue
+						}
+						if err := tx.Commit(); err != nil {
+							_l.Error("Failed tx commit. ", zap.Error(err))
+							skip = true
+							continue
+						}
+						if err := store.SetStatus(orderID, strconv.Itoa(int(bill.Status))); err != nil {
+							_l.Error("Failed set status to external transaction. ", zap.Error(err))
+						}
+						continue
+					}
+				}
+				if err := tx.Rollback(); err != nil {
+					_l.Error("Failed tx rollback. ", zap.Error(err))
+					skip = true
+					continue
+				}
+			}
+		}
+		if skip {
+			continue
+		}
+		afterDate = nextDate
+	}
+	_l.Info("Stoped")
+}
+
+func newPaymentOrderID(kontragentID, billID int64) string {
+	b := make([]byte, 3)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf(
+		"app-md-%d-%d",
+		kontragentID,
+		billID)
+}
--- provider/moedelo/worker.go
+++ provider/moedelo/worker.go
@@ -17,6 +17,8 @@ type Command string
 const (
 	SUBJECT                = "provider_moedelo_subject"
 	CreateBill     Command = "create_bill"
+	HoldBill       Command = "hold_bill"
+	PayBill        Command = "pay_bill"
 	ReverseForHold Command = "reverse_for_hold"
 )
 
@@ -109,7 +111,7 @@ func (p *Provider) NatsHandler() func(m *MessageToMoedelo) {
 				items,
 			)
 			if err != nil {
-				p.l.Error("Failed auth transfer in sberbank.", zap.Error(err))
+				p.l.Error("Failed auth transfer in moedelo.", zap.Error(err))
 				if err := tx.Rollback(); err != nil {
 					p.l.Error("Failed tx rollback. ", zap.Error(err))
 				}
@@ -157,6 +159,212 @@ func (p *Provider) NatsHandler() func(m *MessageToMoedelo) {
 			}
 			if err := tx.Rollback(); err != nil {
 				p.l.Error("Failed tx rollback. ", zap.Error(err))
+			}
+		case HoldBill:
+			tr := engine.Transaction{TransactionID: m.TransactionID}
+			if err := tx.Reload(&tr); err != nil {
+				p.l.Error("Failed reload transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			if tr.Status != engine.AUTH_TX {
+				p.l.Warn(
+					"Transaction status not auth.",
+					zap.Int64("tr_id", tr.TransactionID),
+					zap.String("status", string(tr.Status)),
+				)
+				return
+			}
+			if tr.Meta == nil {
+				p.l.Warn(
+					"Transaction not set meta.",
+					zap.Int64("tr_id", tr.TransactionID),
+				)
+				return
+			}
+			meta := make(map[string]string)
+			if err := json.Unmarshal(*tr.Meta, &meta); err != nil {
+				p.l.Error("Failed unmarshal meta in transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			kontragentID, err := strconv.ParseInt(meta["kontragent_id"], 10, 64)
+			if err != nil {
+				p.l.Error("Failed parse kontragent_id in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			count, err := strconv.ParseFloat(meta["count"], 10)
+			if err != nil {
+				p.l.Error("Failed parse count in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			price, err := strconv.ParseFloat(meta["price"], 10)
+			if err != nil {
+				p.l.Error("Failed parse price in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+
+			items := []SalesDocumentItemModel{
+				{
+					Name:    meta["title"],
+					Count:   count,
+					Unit:    meta["unit"],
+					Type:    Service,
+					Price:   price,
+					NdsType: Nds0,
+				},
+			}
+
+			billID, err := strconv.ParseInt(*tr.ProviderOperID, 10, 64)
+			if err != nil {
+				p.l.Error("Failed parse billID in transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			status := PartiallyPaid
+			err = p.UpdateBill(
+				billID,
+				kontragentID,
+				time.Now(),
+				items,
+				&status,
+			)
+			if err != nil {
+				p.l.Error("Failed update bill from auth transfer in moedelo.", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			statusStr := status.String()
+			tr.ProviderOperStatus = &statusStr
+			if err := tx.Save(&tr); err != nil {
+				p.l.Error("Failed reload transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			if err := tx.Commit(); err != nil {
+				p.l.Error("Failed tx commit. ", zap.Error(err))
+			}
+		case PayBill:
+			tr := engine.Transaction{TransactionID: m.TransactionID}
+			if err := tx.Reload(&tr); err != nil {
+				p.l.Error("Failed reload transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			if tr.Status != engine.HOLD_TX {
+				p.l.Warn(
+					"Transaction status not auth.",
+					zap.Int64("tr_id", tr.TransactionID),
+					zap.String("status", string(tr.Status)),
+				)
+				return
+			}
+			if tr.Meta == nil {
+				p.l.Warn(
+					"Transaction not set meta.",
+					zap.Int64("tr_id", tr.TransactionID),
+				)
+				return
+			}
+			meta := make(map[string]string)
+			if err := json.Unmarshal(*tr.Meta, &meta); err != nil {
+				p.l.Error("Failed unmarshal meta in transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			kontragentID, err := strconv.ParseInt(meta["kontragent_id"], 10, 64)
+			if err != nil {
+				p.l.Error("Failed parse kontragent_id in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			count, err := strconv.ParseFloat(meta["count"], 10)
+			if err != nil {
+				p.l.Error("Failed parse count in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			price, err := strconv.ParseFloat(meta["price"], 10)
+			if err != nil {
+				p.l.Error("Failed parse price in transaction meta. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+
+			items := []SalesDocumentItemModel{
+				{
+					Name:    meta["title"],
+					Count:   count,
+					Unit:    meta["unit"],
+					Type:    Service,
+					Price:   price,
+					NdsType: Nds0,
+				},
+			}
+
+			billID, err := strconv.ParseInt(*tr.ProviderOperID, 10, 64)
+			if err != nil {
+				p.l.Error("Failed parse billID in transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			status := Paid
+			err = p.UpdateBill(
+				billID,
+				kontragentID,
+				time.Now(),
+				items,
+				&status,
+			)
+			if err != nil {
+				p.l.Error("Failed update bill from auth transfer in moedelo.", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			statusStr := status.String()
+			tr.ProviderOperStatus = &statusStr
+			if err := tx.Save(&tr); err != nil {
+				p.l.Error("Failed reload transaction. ", zap.Error(err))
+				if err := tx.Rollback(); err != nil {
+					p.l.Error("Failed tx rollback. ", zap.Error(err))
+				}
+				return
+			}
+			if err := tx.Commit(); err != nil {
+				p.l.Error("Failed tx commit. ", zap.Error(err))
 			}
 		case ReverseForHold:
 			tx, err := p.db.Begin()
