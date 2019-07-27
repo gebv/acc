38dec1ad5805e452c3ad26b47acd2ef5e307f0cd
AndreyDyubin <d.andrey@fader.site>
2019-07-27 09:30:23 +0300

    добавляет метод BalanceChanges

--- services/accounts/service.go
+++ services/accounts/service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gebv/acca/api"
 	"github.com/gebv/acca/engine"
+	"github.com/gebv/acca/services/invoices"
 	"google.golang.org/grpc/codes"
 	"gopkg.in/reform.v1"
 
@@ -78,6 +79,72 @@ func (s *Server) GetAccountByKey(ctx context.Context, req *api.GetAccountByKeyRe
 			Meta:            acc.Meta,
 			BalanceAccepted: acc.BalanceAccepted,
 		},
+	}, nil
+}
+
+func (s *Server) BalanceChanges(ctx context.Context, req *api.BalanceChangesRequest) (*api.BalanceChangesResponse, error) {
+
+	list, err := s.db.SelectAllFrom(engine.ViewBalanceChangesView, "OFFSET $1 LIMIT $2", req.GetOffset(), req.GetLimit())
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed get balance_changes.")
+	}
+	balanceChanges := make([]*api.BalanceChanges, 0, len(list))
+	for _, v := range list {
+		vbc := v.(*engine.ViewBalanceChanges)
+		var operations []*api.BalanceChanges_Operation
+		if vbc.Operations != nil {
+			operations = make([]*api.BalanceChanges_Operation, 0, len(*vbc.Operations))
+			for _, op := range *vbc.Operations {
+				operations = append(operations, &api.BalanceChanges_Operation{
+					OperId:    op.OperID,
+					SrcAccId:  op.SrcAccID,
+					DstAccId:  op.DstAccID,
+					Strategy:  invoices.MapOperStrategyToApiTrStrategy[op.Strategy],
+					Key:       op.Key,
+					Hold:      op.Hold,
+					HoldAccId: op.HoldAccID,
+					Status:    invoices.MapOperStatusToApiTrStatus[op.Status],
+				})
+			}
+		}
+		balanceChanges = append(balanceChanges, &api.BalanceChanges{
+			ChId:            vbc.ChID,
+			TxId:            vbc.TxID,
+			AccId:           vbc.AccID,
+			Amount:          vbc.Amount,
+			Balance:         vbc.Balance,
+			BalanceAccepted: vbc.BalanceAccepted,
+			Account: &api.BalanceChanges_Account{
+				AccId:           vbc.Account.AccID,
+				Key:             vbc.Account.Key,
+				Balance:         vbc.Account.Balance,
+				BalanceAccepted: vbc.Account.BalanceAccepted,
+			},
+			Currency: &api.BalanceChanges_Currency{
+				CurrId: vbc.Currency.CurrID,
+				Key:    vbc.Currency.Key,
+			},
+			Invoice: &api.BalanceChanges_Invoice{
+				InvoiceId: vbc.Invoice.InvoiceID,
+				Key:       vbc.Invoice.Key,
+				Strategy:  vbc.Invoice.Strategy,
+				Status:    invoices.MapInvStatusToApiInvStatus[vbc.Invoice.Status],
+			},
+			Transaction: &api.BalanceChanges_Transaction{
+				TxId:               vbc.Transaction.TxID,
+				Key:                vbc.Transaction.Key,
+				Strategy:           vbc.Transaction.Strategy,
+				Status:             invoices.MapTrStatusToApiTrStatus[vbc.Transaction.Status],
+				Provider:           invoices.MapTrProviderToApiTrProvider[vbc.Transaction.Provider],
+				ProviderOperId:     vbc.Transaction.ProviderOperID,
+				ProviderOperStatus: vbc.Transaction.ProviderOperStatus,
+				ProviderOperUrl:    vbc.Transaction.ProviderOperUrl,
+			},
+			Operations: operations,
+		})
+	}
+	return &api.BalanceChangesResponse{
+		BalanceChanges: balanceChanges,
 	}, nil
 }
 
