efdd7f8d7bc42422540050ee16498a84358beff3
AndreyDyubin <d.andrey@fader.site>
2019-08-12 23:16:34 +0300

    изменяет схему БД, добавляет вьюшки для инвойсов и транзакций

---
+++ postgres_schema/2019-08-12_01_views.sql
@@ -0,0 +1,55 @@
+BEGIN;
+
+ALTER TABLE acca.invoices
+    DROP COLUMN amount;
+
+CREATE OR REPLACE VIEW acca.v_transactions as
+SELECT t.*,
+       JSON_AGG(
+               JSON_BUILD_OBJECT(
+                       'oper_id', o.oper_id,
+                       'tx_id', o.tx_id,
+                       'invoice_id', o.invoice_id,
+                       'src_acc_id', o.src_acc_id,
+                       'dst_acc_id', o.dst_acc_id,
+                       'strategy', o.strategy,
+                       'amount', o.amount,
+                       'key', o.key,
+                       'meta', o.meta,
+                       'hold', o.hold,
+                       'hold_acc_id', o.hold_acc_id,
+                       'status', o.status,
+                       'created_at', o.created_at,
+                       'updated_at', o.updated_at
+                   )
+           ) AS operations
+FROM transactions AS t
+         INNER JOIN operations AS o USING (tx_id)
+GROUP BY 1;
+
+CREATE OR REPLACE VIEW acca.v_invoices as
+SELECT i.*,
+       JSON_AGG(
+               JSON_BUILD_OBJECT(
+                       'tx_id', t.tx_id,
+                       'invoice_id', t.invoice_id,
+                       'key', t.key,
+                       'strategy', t.strategy,
+                       'amount', t.amount,
+                       'provider', t.provider,
+                       'provider_oper_id', t.provider_oper_id,
+                       'provider_oper_status', t.provider_oper_status,
+                       'provider_oper_url', t.provider_oper_url,
+                       'meta', t.meta,
+                       'status', t.status,
+                       'next_status', t.next_status,
+                       'updated_at', t.updated_at,
+                       'created_at', t.created_at,
+                       'operations', t.operations
+                   )
+           ) AS transactions
+FROM invoices AS i
+         INNER JOIN v_transactions AS t USING (invoice_id)
+GROUP BY 1;
+
+COMMIT;
