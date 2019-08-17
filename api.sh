c88b0ff54362ea921677615eafe2dfb392f6a1d0
AndreyDyubin <d.andrey@fader.site>
2019-08-17 10:46:12 +0300

    обновляет api.sh

--- api.sh
+++ api.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -x
 
 # ./api.sh '{"key":"curr"}' "api.Accounts/CreateCurrency"
+# ./api.sh '{"key":"curr"}' "api.Accounts/GetCurrency"
+# ./api.sh '{}' "api.Updates/GetUpdate"
 
 grpcurl \
   -rpc-header 'x-request-id: api-sh-test-123' \
-  -rpc-header 'device-id: api.sh' \
+  -rpc-header 'access-token: cfb25eb22addb7d7edb743388a0f6b406e3fea368f2dd742c18a6a816791d74e' \
   -plaintext \
   -import-path ./ \
   -proto ./api/accounts.proto \
   -proto ./api/invoice.proto \
+  -proto ./api/update.proto \
   -import-path ./vendor \
   -proto github.com/mwitkow/go-proto-validators/validator.proto \
   -proto github.com/gogo/protobuf/gogoproto/gogo.proto \
@@ -19,4 +22,3 @@ grpcurl \
   -d $1 \
   127.0.0.1:10001 \
   $2
-
